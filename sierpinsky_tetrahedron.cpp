0ca284607d2551ab7a616e83ae06756dae5b0763
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-04-14 23:20:32 +0200

    Added random colors to the triangle

--- sierpinsky_tetrahedron.cpp
+++ sierpinsky_tetrahedron.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <iostream>
 #include <map>
+#include <cmath>
 
 struct Normal
 compute_normal (struct Vector a, struct Vector b)
@@ -10,6 +11,11 @@ compute_normal (struct Vector a, struct Vector b)
 	result.components[0] = a.y * b.z - a.z * b.y;
 	result.components[1] = a.z * b.x - a.x * b.z;
 	result.components[2] = a.y * b.y - a.y * b.x;
+
+	GLfloat length = sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+	result.components[0] /= length;
+	result.components[1] /= length;
+	result.components[2] /= length;
 
 	return result;
 }
@@ -21,6 +27,18 @@ operator-(struct Point a, struct Point b)
 	result.x = a.components[0] - b.components[0];
 	result.y = a.components[1] - b.components[1];
 	result.z = a.components[2] - b.components[2];
+
+	return result;
+}
+
+struct Color
+random_color ()
+{
+	struct Color result;
+	result.components[0] = (GLfloat)(rand() % 1000) / (GLfloat)999.0;
+	result.components[1] = (GLfloat)(rand() % 1000) / (GLfloat)999.0;
+	result.components[2] = (GLfloat)(rand() % 1000) / (GLfloat)999.0;
+	result.components[3] = (GLfloat)0.5;
 
 	return result;
 }
@@ -45,11 +63,24 @@ create_sierpinsky_tetrahedron(struct Tetrahedron base, unsigned int depth)
 		if (top.second == 0)
 		{
 			struct Normal n;
+			struct Color color;
+			struct TexCoord tc1 {{0.5, 0.0}};
+			struct TexCoord tc2 {{0.0, 1.0}};
+			struct TexCoord tc3 {{1.0, 1.0}};
+			
+			a.tex_coord = tc1;
+			b.tex_coord = tc2;
+			c.tex_coord = tc3;
+			d.tex_coord = tc1;
 
 			n = compute_normal(a - b, a - c); 
 			a.normal = n;
 			b.normal = n;
 			c.normal = n;
+			color = random_color();
+			a.color = color;
+			b.color = color;
+			c.color = color;
 			struct Triangle t1 {a, b, c};
 			result.push_back(t1);
 
@@ -57,6 +88,10 @@ create_sierpinsky_tetrahedron(struct Tetrahedron base, unsigned int depth)
 			a.normal = n;
 			b.normal = n;
 			d.normal = n;
+			color = random_color();
+			a.color = color;
+			b.color = color;
+			d.color = color;
 			struct Triangle t2 {a, b, d};
 			result.push_back(t2);
 
@@ -64,6 +99,10 @@ create_sierpinsky_tetrahedron(struct Tetrahedron base, unsigned int depth)
 			a.normal = n;
 			d.normal = n;
 			c.normal = n;
+			color = random_color();
+			a.color = color;
+			d.color = color;
+			c.color = color;
 			struct Triangle t3  {a, d, c};
 			result.push_back(t3);
 
@@ -71,6 +110,10 @@ create_sierpinsky_tetrahedron(struct Tetrahedron base, unsigned int depth)
 			b.normal = n;
 			c.normal = n;
 			d.normal = n;
+			color = random_color();
+			b.color = color;
+			c.color = color;
+			d.color = color;
 			struct Triangle t4 {b, c, d};
 			result.push_back(t4);
 		}
