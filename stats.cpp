2f7a6496ea3980cd295cb8ebe5627ebb241f1f59
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-04-19 21:32:57 +0200

    Removed unuseful modus

--- stats.cpp
+++ stats.cpp
@@ -4,37 +4,6 @@
 #include <numeric>
 #include <algorithm>
 
-long
-longest_running_sequence (std::vector<long>& data)
-{
-	long result = data[0];	
-	int longest = 1;
-
-	for (int i = 0; i < data.size(); i++)
-	{
-		int actual;
-		long start; 
-		
-		start = data[i];
-		actual = 0;
-		i++;
-		if (data[i] == start)
-		{
-			actual++;
-			i++;
-		}
-		else
-		{
-			if (actual > longest)
-			{
-				longest = actual;
-				result = start;
-			}
-		}
-	}
-
-	return result;
-}
 
 void 
 print_stats (std::string action, std::vector<long>& data)
@@ -45,12 +14,10 @@ print_stats (std::string action, std::vector<long>& data)
 	double average = sum / data.size();
 	std::sort(data.begin(), data.end());
 	double median = data[data.size()/2];
-	long modus = longest_running_sequence(data);
 
 	std::cout << "Minimal " << action << " time: " << minimal << std::endl;
 	std::cout << "Average " << action << " time: " << average << std::endl;
 	std::cout << "Maximal " << action << " time: " << maximal << std::endl;
 	std::cout << "Median " << action << " time: " << median << std::endl;
-	std::cout << "Modus " << action << " time: " << median << std::endl;
 }
 
