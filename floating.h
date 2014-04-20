eb85cbc7e1ee9fb133cf565a396d2adae852a1ac
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-04-20 12:47:46 +0200

    Extended the definitions of floating types

--- floating.h
+++ floating.h
@@ -1,10 +1,20 @@
 #ifdef TEST_DOUBLE
 	#define MyFloating GLdouble
 	#define MY_FLOATING GL_DOUBLE
+	#define TypeName "double"
+	#define myTexCoord2v glTexCoord2dv
+	#define myColor4v glColor4dv
+	#define myNormal3v glNormal3dv
+	#define myVertex3v glVertex3dv
 #endif
 
 #ifdef TEST_FLOAT
 	#define MyFloating GLfloat
 	#define MY_FLOATING GL_FLOAT
+	#define TypeName "float"
+	#define myTexCoord2v glTexCoord2fv
+	#define myColor4v glColor4fv
+	#define myNormal3v glNormal3fv
+	#define myVertex3v glVertex3fv
 #endif
 
