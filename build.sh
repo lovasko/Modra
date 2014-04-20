2e5e272c8e60e70f3e01813110de5f173ffc5186
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-04-20 12:43:53 +0200

    Added builgin float/double binaries

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-clang++ -o test -std=c++11 main.cpp sierpinsky_tetrahedron.cpp utilities.cpp fractal_helpers.cpp menger_sponge.cpp options.cpp stats.cpp -I/opt/local/include -L/opt/local/lib -lGLEW -lSDL -lSDLmain -framework OpenGL -framework Cocoa
+clang++ -DTEST_FLOAT -o testf -std=c++11 main.cpp sierpinsky_tetrahedron.cpp utilities.cpp fractal_helpers.cpp menger_sponge.cpp options.cpp stats.cpp -I/opt/local/include -L/opt/local/lib -lGLEW -lSDL -lSDLmain -framework OpenGL -framework Cocoa
+
+clang++ -DTEST_DOUBLE -o testd -std=c++11 main.cpp sierpinsky_tetrahedron.cpp utilities.cpp fractal_helpers.cpp menger_sponge.cpp options.cpp stats.cpp -I/opt/local/include -L/opt/local/lib -lGLEW -lSDL -lSDLmain -framework OpenGL -framework Cocoa
 
 clang++ -o info -std=c++11 info_main.cpp opengl_properties.cpp -I/opt/local/include -L/opt/local/lib -lGLEW -framework OpenGL -framework Cocoa
 
